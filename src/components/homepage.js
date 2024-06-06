ebcf920fd6348d0d1bd092454a1932508114ba20
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-06-06 23:31:01 +0530

    made the home page

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,7 +1,25 @@
+import "../style/homepage.css"
+import bookPng from "../assets/tea and book.png"
 const Home = () => {
     return ( 
-        <div className="home-component">
-            
+        <div className="home-container">
+            <div className="home-component">
+                <div className="home-description">
+                    <h1>
+                        Discover your Next Read
+                    </h1>  
+                    <h3>
+                        Buy and sell pre-loved books effortlessly. Explore, enjoy, and connect with fellow book lovers at BookNest!
+                    </h3>
+                    <input type="button" value="explore" />
+                </div>
+                <div className="book-image">
+                    <img src={bookPng} alt="" srcset="" />
+                </div>
+            </div>
+            <div className="home-footer-title">
+                <p>Scroll Down</p>
+            </div>
         </div>
      );
 }

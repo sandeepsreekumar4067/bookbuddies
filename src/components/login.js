9d303917e0881c5f75adf43b9498867813b3d28c
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-06-08 13:57:14 +0530

    added the navigation bar

--- src/components/login.js
+++ src/components/login.js
@@ -1,8 +1,24 @@
 import "../style/login.css"
+import loginImage from "../assets/loginImage.png"
+import NavigationBar from "./navigationbar";
 const Login = () => {
     return ( 
         <div className="login-container">
-            Helo
+            <NavigationBar/>
+            <div className="login-components">
+                <div className="image-container">
+                    <img src={loginImage} alt="" />
+                    <div className="gradient">
+                        <h1>Login</h1>
+                        <h2>Welcome Back</h2>
+                    </div>
+                </div>
+                <div className="login-input-container">
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password"  />
+                    <input type="button" value="Login" />
+                </div>
+            </div>
         </div>
      );
 }

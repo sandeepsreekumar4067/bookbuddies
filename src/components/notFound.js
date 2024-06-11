1d3b36c07ac0b47887478b24772ee17369be5b11
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-06-11 16:36:22 +0530

    added not found component

---
+++ src/components/notFound.js
@@ -0,0 +1,11 @@
+import "../style/notFound.css"
+const NotFound = () => {
+    return ( 
+        <div className="not-found-container">
+            <h1>Error 404</h1>
+            <h3>Page not Found</h3>
+        </div>
+     );
+}
+ 
+export default NotFound;

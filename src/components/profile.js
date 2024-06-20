c1c97e5302972199fa8d5b786c84c4bd875bfef1
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-06-20 21:27:28 +0530

    added the profile page . still on progress ...

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,6 +1,21 @@
+import "../style/profile.css"
 const Profile = () => {
     return ( 
         <div className="profile-container">
+            <div className="profile-title">
+                Welcome Sandeep Sreekumar
+            </div>
+            <div className="profile-section">
+                <div className="bookAdd-section">
+                    <input type="text" placeholder="Enter the name of the Book" />
+                    <input type="number" placeholder="Enter the Price of the Book" />
+                    <textarea name="" id="" placeholder="Contact details"/>
+                    <input type="button" value="Submit" />
+                </div>
+                <div className="bookView-section">
+
+                </div>
+            </div>
         </div>
      );
 }
